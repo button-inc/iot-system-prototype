7d5a35b27bbbd090b89f243e482b230764374c2d
YaokunLin <yaokun1@ualberta.ca>
2023-08-16 10:05:14 -0700

    removed some hard coded values and improve UI

--- app/components/SensorMarker.tsx
+++ app/components/SensorMarker.tsx
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import LinearProgress from "@mui/material/LinearProgress";
 import Box from "@mui/material/Box";
 import Image from 'next/image';
+
 interface Props {
   sensorToMark: Sensor;
   fillLevelThreshold?: number;
@@ -24,34 +25,62 @@ const SensorMarker: React.FC<Props> = ({
       : "healthy";
 
   let iconUrl;
+  let linearProgressColor : "error" | "inherit" | "success" | "primary" | "secondary" | "info" | "warning";
   switch (iconName) {
     case "error":
       iconUrl = "https://cdn-icons-png.flaticon.com/128/1304/1304037.png";
+      linearProgressColor = "error";
       break;
     case "full":
       iconUrl = "https://cdn-icons-png.flaticon.com/128/5028/5028066.png";
+      linearProgressColor = "error";
       break;
     case "healthy":
       iconUrl = "https://cdn-icons-png.flaticon.com/128/542/542775.png";
+      linearProgressColor = "success";
       break;
     default:
       iconUrl = "https://cdn-icons-png.flaticon.com/128/484/484662.png";
+      linearProgressColor = "primary";
   }
   const maekerIcon = new L.Icon({
     iconUrl: iconUrl,
     iconSize: [25, 25],
   });
 
-  const fill_level1 = 50;
-  const bin_vol1 = 100;
-  const fill_pct =  Math.round(fill_level1 / bin_vol1 * 100);
 
+  const fill_pct =  (fillLevelThreshold === undefined || fill_level === null)? null : Math.round(fill_level / fillLevelThreshold * 100);
+  console.log({fill_pct, fill_level, fillLevelThreshold})
+  
   return (
     <Marker key={id} position={[lat, long]} icon={maekerIcon}>
       <Popup>
           <div style={{ padding: '5px', display: 'flex', width: '300px' }}>
               <div style={{ flex: 2 }}> {/* Left column*/}
 
+              {(fill_pct===null) && 
+                <Box 
+                  style={{
+                      margin: 'auto', 
+                      width: '40px',
+                      height: '65%',
+                      display: 'flex', 
+                      flexDirection: 'column', 
+                      alignItems: 'center', 
+                      justifyContent: 'center'
+                  }}
+                  >
+                    <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ErrorIcon"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg>
+                    <Typography 
+                      variant="subtitle2" 
+                      style={{ marginTop: '2px', whiteSpace: 'nowrap', }}
+                    >
+                      level not captured
+                    </Typography>
+                  </Box> 
+                }
+
+              {fill_pct && 
                 <Box 
                   style={{
                       margin: 'auto', width: '40px',
@@ -61,30 +90,32 @@ const SensorMarker: React.FC<Props> = ({
                       justifyContent: 'center'
                   }}
                   >
-                  <Typography 
-                    variant="body1" 
-                    style={{ marginBottom: '38px', marginTop: '5px', }}
-                  >
-                    {`${fill_pct}%` ?? "Error"}
-                  </Typography>
+                    <Typography 
+                      variant="body1" 
+                      style={{ marginBottom: '38px', marginTop: '5px', }}
+                    >
+                      {`${fill_pct}%`}
+                    </Typography>
                   
                   <LinearProgress 
                       variant="determinate" 
-                      value={fill_pct} 
+                      value={fill_pct}
+                      color={linearProgressColor}
                       style={{ 
                         transform: 'rotate(270deg)', 
                         width: '100px', 
                         height:'20px', 
                         borderRadius: '0px 5px 5px 0px',  }}
-                  />
+                    />
+
                   <Typography 
                     variant="body1" 
                     style={{ whiteSpace: 'nowrap',marginTop: '38px'}}  
-                  >
+                    >
                       Fill level
                   </Typography>
         
-                </Box>
+                </Box>}
 
 
                 {/* refactor needed */}
