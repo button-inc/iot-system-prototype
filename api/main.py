2e99ed2ca4eceefb07b9833d2b9d50ef289aa89b
Gabriel Lopes <gabrielrrlopes@gmail.com>
2023-08-18 17:19:47 -0400

    Added hardcoded app and api

--- api/main.py
+++ api/main.py
@@ -16,6 +16,8 @@ else:
     REAL_FAKE_SENSORS_BASE_URL = "http://localhost:8081"
     SENSATIONAL_SENSORS_BASE_URL = "http://localhost:8082"
 
+REAL_FAKE_SENSORS_BASE_URL = "http://real-fake-service:8081"
+SENSATIONAL_SENSORS_BASE_URL = "http://sensational-sensor-service:8082"
 
 app = FastAPI()
 
@@ -133,12 +135,12 @@ def get_sensors():
     real_fake_sensors = requests.get(REAL_FAKE_SENSORS_BASE_URL + "/sensors").json()
     rfs_as_simple_sensor_list = rfs_list_to_bs_list(real_fake_sensors["sensors"])
 
-    sensational_sensors = requests.get(SENSATIONAL_SENSORS_BASE_URL + "/sensors").json()
-    ss_as_simple_sensor_list = sensational_sensor_list_to_simple_sensor_list(
-        sensational_sensors["sensors"]
-    )
+    # sensational_sensors = requests.get(SENSATIONAL_SENSORS_BASE_URL + "/sensors").json()
+    # ss_as_simple_sensor_list = sensational_sensor_list_to_simple_sensor_list(
+    #     sensational_sensors["sensors"]
+    # )
 
-    all_sensors_list = rfs_as_simple_sensor_list + ss_as_simple_sensor_list
+    all_sensors_list = rfs_as_simple_sensor_list #+ ss_as_simple_sensor_list
     return {"total": len(all_sensors_list), "sensors": all_sensors_list}
 
 
