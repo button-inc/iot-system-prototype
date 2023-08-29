2423ebb631326021262fde5aaf691c998b577efe
monicakochofar <monicakochofar@gmail.com>
2023-08-29 16:41:55 -0400

    converting certain variables to ref

--- app_vue/src/pages/MapPage.vue
+++ app_vue/src/pages/MapPage.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import SensorMap from '@/components/sensorMap.vue';
-import data from '@/data/sensorsMock.json';
+  import { ref } from 'vue';
+  import SensorMap from '@/components/sensorMap.vue';
+  import data from '@/data/sensorsMock.json';
 
-const sensors = ref(data.sensors);
-const alertThreshold = 50;
-const filterThresholdMaximum = 100;
-const filterThresholdMinimum = 0;
-
+  const sensors = ref(data.sensors);
+  const alertThreshold = ref(50);
+  const filterThresholdMaximum = ref(100);
+  const filterThresholdMinimum = ref(0);
 </script>
 
 <template>
