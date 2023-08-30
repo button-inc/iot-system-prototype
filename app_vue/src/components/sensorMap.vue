2ef4741dd09eead830b8e55ef1991ce05e19252c
monicakochofar <monicakochofar@gmail.com>
2023-08-30 19:07:36 -0400

    adding mobile view for sidebar and map

--- app_vue/src/components/sensorMap.vue
+++ app_vue/src/components/sensorMap.vue
@@ -1,8 +1,9 @@
 <script setup>
-  import { ref } from 'vue'
+  import { reactive, ref, onMounted } from 'vue'
   import SensorMapMarker from '@/components/sensorMapMarker.vue';
   import 'leaflet/dist/leaflet.css'
   import { LMap, LTileLayer, LMarker, LControlZoom } from '@vue-leaflet/vue-leaflet'
+  import { useDevice, DEVICE_SIZE } from '@/utils/screenSizeHelper';
 
   const props = defineProps({
     sensors: {
@@ -22,12 +23,30 @@
 
   const center = ref([43.7, -79.42]);
   const zoom = ref(10);
+
+  const state = reactive({
+    location: 'bottomright',
+    device: useDevice()
+  });
+
+  onMounted(() => {
+    window.addEventListener("resize", onWindowResize);
+  });
+
+  function onWindowResize() {
+    state.device = useDevice();
+    if (state.device.size === DEVICE_SIZE.s || state.device.size === DEVICE_SIZE.xs) {
+      state.location = 'topright';
+    } else {
+      state.location = 'bottomright';
+    }
+  }
 </script>
 
 <template>
   <div v-if="sensors" class="sensor-map-container">
     <l-map ref="map" v-model:zoom="zoom" :use-global-leaflet="false" :center="center" :options="{zoomControl: false}">
-      <l-control-zoom position="bottomright"/>
+      <l-control-zoom :position="state.location"/>
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
