b43e5e50e501ec1553f0ee585a12cd96c0051b09
monicakochofar <monicakochofar@gmail.com>
2023-08-31 20:44:57 -0400

    working with vuetify to design two overlaying sidebars, had to use some importants and :deep to accomplish. also added a map link

--- app_vue/src/layout/navigationBar.vue
+++ app_vue/src/layout/navigationBar.vue
@@ -11,21 +11,25 @@
   <div>
     <v-navigation-drawer
       id="navigation"
-      :expand-on-hover="expand"
+      :expand-on-hover="false"
+      :rail="!expand"
       :key="expand"
       color="#191A1C"
       rail-width="41"
       width="400"
-      rail
       permanent
     >
-      <v-list class="navbar-icon" :class="{'navbar-icon__align-right': expand}" @click="expandNavBar">
+      <v-list class="navbar-icon" :class="{'navbar-icon__x': expand}" @click="expandNavBar">
         <vue-feather v-show="!expand" type="align-justify"></vue-feather>
         <vue-feather v-show="expand" type="x"></vue-feather>
       </v-list>
 
-      <v-list density="compact" nav>
-        <v-list-item title="Map" value="myfiles"></v-list-item>
+      <v-list v-show="expand" class="navbar-links" density="compact" nav>
+        <v-list-item class="navbar-links__link" link href="/" rounded value="Map">
+          <vue-feather v-show="expand" type="map"></vue-feather>
+          Map
+        </v-list-item>
+        <v-divider class="mt-3 mb-3"></v-divider>
       </v-list>
 
     </v-navigation-drawer>
@@ -37,7 +41,25 @@
 </template>
 
 <style lang="scss" scoped>
+  // vuetify overrides
+  :deep .v-list.navbar-icon {
+    padding: 14px 0;
+  }
+  :deep .navbar-links .v-list-item__content {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
 
+  :deep .navbar-links.v-list {
+    padding: 0 40px 40px 40px;
+  }
+
+  :deep .v-list-item.navbar-links__link {
+    padding: 6px 16px;
+  }
+
+  // custom css
   .navigation-overlay {
     background-color: black;
     opacity: 0.2;
@@ -53,8 +75,9 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
 
-    &__align-right {
+    &__x {
       justify-content: flex-end;
       margin-right: 8px;
     }
