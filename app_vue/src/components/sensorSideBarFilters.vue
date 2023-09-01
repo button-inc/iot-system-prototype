ecdaa1950d33a1eb683c4ead84040149bdd7374b
monicakochofar <monicakochofar@gmail.com>
2023-08-31 21:49:02 -0400

    completing clear functionality

--- app_vue/src/components/sensorSideBarFilters.vue
+++ app_vue/src/components/sensorSideBarFilters.vue
@@ -1,42 +1,60 @@
 <script setup>
-  import { reactive, ref } from 'vue'
+  import { reactive, ref, watch, onMounted } from 'vue'
   import { useSensorStore } from '@/stores/sensors_store';
+
+
+
 
   const thresholdRange = ref([0,100]);
 
   const sensorStore = useSensorStore();
+
+  
   
   const state = reactive({
-    sliderValue: [0, 100],
+    selectedGroup: null,
+    selectedAssetTag: [],
+    selectedBinType: null,
+    selectedBinVolume: null,
+    selectedFillRange: [0, 100],
     group: sensorStore.getAllGroupOptions(),
     assetTag: sensorStore.getAllAssetTags(),
     binType: sensorStore.getAllBinTypes(),
-    binVolume: sensorStore.getAllBinVolumes()
+    binVolume: sensorStore.getAllBinVolumes(),
   });
 
-  function updateGroupFilter(group) {
-    sensorStore.setSelectedGroup(group);
+  function updateGroupFilter() {
+    sensorStore.setSelectedGroup(state.selectedGroup);
     sensorStore.updateSensorsWithFilters();
   }
 
-  function updateAssetTagFilter(assetTag) {
-    sensorStore.setSelectedAssetTag(assetTag);
+  function updateAssetTagFilter() {
+    sensorStore.setSelectedAssetTag(state.selectedAssetTag);
     sensorStore.updateSensorsWithFilters();
   }
 
-  function updateBinTypeFilter(binType) {
-    sensorStore.setSelectedBinType(binType);
+  function updateBinTypeFilter() {
+    sensorStore.setSelectedBinType(state.selectedBinType);
     sensorStore.updateSensorsWithFilters();
   }
 
-  function updateBinVolumeFilter(binVolume) {
-    sensorStore.setSelectedBinVolume(binVolume);
+  function updateBinVolumeFilter() {
+    sensorStore.setSelectedBinVolume(state.selectedBinVolume);
     sensorStore.updateSensorsWithFilters();
   }
 
-  function updateFillRangeFilter(fillRange) {
-    sensorStore.setSelectedFillRange(fillRange);
+  function updateFillRangeFilter() {
+    sensorStore.setSelectedFillRange(state.selectedFillRange);
     sensorStore.updateSensorsWithFilters();
+  }
+
+  function clearFilters() {
+    sensorStore.$reset();
+    state.selectedGroup = null;
+    state.selectedAssetTag = null;
+    state.selectedBinType = null;
+    state.selectedBinVolume = null;
+    state.selectedFillRange = [0, 100];
   }
 
 </script>
@@ -46,13 +64,13 @@
     <div class="text-h6 padding-b-30 d-flex align-center">
       <span>Filter Sensors ({{ sensorStore.getTotalSensors() }})</span> 
       <span class="mx-3">|</span>
-      <v-btn variant="plain" color="#2196F3" class="pa-0 pt-1 text-capitalize" @click="sensorStore.$reset()">Clear</v-btn>
+      <v-btn variant="plain" color="#2196F3" class="pa-0 pt-1 text-capitalize" @click="clearFilters">Clear</v-btn>
     </div>
 
     <div class="filter-list__fill-level">
       <div class="filter-list__label color-gray-grey">Fill Level Threshold</div>
       <v-range-slider class="filter-list__slider" 
-        v-model="state.sliderValue"
+        v-model="state.selectedFillRange"
         strict
         color="#2196F3"
         track-color="#2196F3"
@@ -70,12 +88,14 @@
     </div>
 
     <v-select class="filter-list__dropdown"
+      v-model="state.selectedGroup"
       label="Group"
       :items="state.group"
       @update:modelValue="updateGroupFilter"
     ></v-select>
 
     <v-select class="filter-list__dropdown"
+      v-model="state.selectedAssetTag"
       label="Asset Tag"
       chips
       multiple
@@ -84,12 +104,14 @@
     ></v-select>
 
     <v-select class="filter-list__dropdown"
+      v-model="state.selectedBinType"
       label="Bin Type"
       :items="state.binType"
       @update:modelValue="updateBinTypeFilter"
     ></v-select>
 
     <v-select class="filter-list__dropdown"
+      v-model="state.selectedBinVolume"
       label="Bin Volume"
       :items="state.binVolume"
       @update:modelValue="updateBinVolumeFilter"
