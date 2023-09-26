21d386145e0a6ab56f847c94e02ae0ba37279eeb
monicakochofar <monicakochofar@gmail.com>
2023-09-26 17:29:44 -0400

    updating prettier to keep semicolons

--- app_vue/src/components/sensorRouteBlock.vue
+++ app_vue/src/components/sensorRouteBlock.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { reactive, onMounted, watch } from 'vue'
-import { useRouteStore } from '@/stores/route_store'
-import { v4 as uuidv4 } from 'uuid'
-import { getMinutesString, getKmFromMeterString } from '@/utils/formattingHelper'
-import draggable from 'vuedraggable'
+import { reactive, onMounted, watch } from 'vue';
+import { useRouteStore } from '@/stores/route_store';
+import { v4 as uuidv4 } from 'uuid';
+import { getMinutesString, getKmFromMeterString } from '@/utils/formattingHelper';
+import draggable from 'vuedraggable';
 
 const props = defineProps({
   selectedRouteList: {
@@ -18,59 +18,59 @@ const props = defineProps({
     type: String,
     required: true
   }
-})
+});
 
 // stores
-const routeStore = useRouteStore()
+const routeStore = useRouteStore();
 
 const state = reactive({
   startPointAddress: '',
   endPointAddress: '',
   drag: false,
   selectedRouteList: []
-})
+});
 
 onMounted(() => {
-  state.startPointAddress = props.startPointAddress
-  state.endPointAddress = props.endPointAddress
-  state.selectedRouteList = props.selectedRouteList
-})
+  state.startPointAddress = props.startPointAddress;
+  state.endPointAddress = props.endPointAddress;
+  state.selectedRouteList = props.selectedRouteList;
+});
 
 watch(
   () => props.selectedRouteList,
   (value) => {
-    state.selectedRouteList = value
+    state.selectedRouteList = value;
   }
-)
+);
 
 function draggedRoute() {
-  state.drag = false
-  routeStore.setSelectedRouteList(state.selectedRouteList) // update state with new route order
-  routeStore.googUpdateRouteStats()
+  state.drag = false;
+  routeStore.setSelectedRouteList(state.selectedRouteList); // update state with new route order
+  routeStore.googUpdateRouteStats();
 }
 
 function exportRouteClicked() {
-  let csv = ''
-  const duration = routeStore.getRouteDuration || ''
-  const distance = routeStore.getRouteDistance || 0
+  let csv = '';
+  const duration = routeStore.getRouteDuration || '';
+  const distance = routeStore.getRouteDistance || 0;
 
-  csv += 'Duration,Distance\n'
+  csv += 'Duration,Distance\n';
 
   if (duration || distance) {
-    const formattedDuration = getMinutesString(duration)
-    const formattedDistance = getKmFromMeterString(distance)
+    const formattedDuration = getMinutesString(duration);
+    const formattedDistance = getKmFromMeterString(distance);
     csv += `${formattedDuration ? formattedDuration : ''},${
       formattedDistance ? formattedDistance + 'km' : ''
-    }\n`
+    }\n`;
   } else {
-    csv += '\n'
+    csv += '\n';
   }
 
-  csv += '\n'
+  csv += '\n';
 
   // columns -- NOTE: order is important
   csv +=
-    'Route Order,Sensor Type,Fill Level,Latitude,Longitude,Manufacturer,Bin Name,Address Line 1,Address Line 2,Group,Bin Type,Material Type,Asset Tag,Bin Volume\n'
+    'Route Order,Sensor Type,Fill Level,Latitude,Longitude,Manufacturer,Bin Name,Address Line 1,Address Line 2,Group,Bin Type,Material Type,Asset Tag,Bin Volume\n';
 
   // grab required data to be exported
   const csvObjectArray = state.selectedRouteList.map((sensor, index) => {
@@ -90,21 +90,21 @@ function exportRouteClicked() {
       material_type: sensor.material_type,
       asset_tag: sensor.asset_tag,
       bin_volume: sensor.bin_volume
-    }
-  })
+    };
+  });
 
   // convert JSON to csv rows
   csvObjectArray.forEach((sensor) => {
-    const csvRow = Object.values(sensor).toString() // outputs json to a value string separated by commas
-    csv += csvRow + '\n'
-  })
+    const csvRow = Object.values(sensor).toString(); // outputs json to a value string separated by commas
+    csv += csvRow + '\n';
+  });
 
   // export
-  const anchor = document.createElement('a')
-  anchor.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv)
-  anchor.target = '_blank'
-  anchor.download = `Route-${uuidv4()}.csv`
-  anchor.click()
+  const anchor = document.createElement('a');
+  anchor.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
+  anchor.target = '_blank';
+  anchor.download = `Route-${uuidv4()}.csv`;
+  anchor.click();
 }
 </script>
 
