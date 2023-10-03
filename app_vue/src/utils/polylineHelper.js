682b1e4ee5d3535efa42979e2e97707a770edd89
monicakochofar <monicakochofar@gmail.com>
2023-10-03 16:57:05 -0400

    google optimize routes api now returns a an encoded polyline, adding decode polyline helper

--- app_vue/src/utils/polylineHelper.js
+++ app_vue/src/utils/polylineHelper.js
@@ -1,13 +1,5 @@
-import polyUtil from 'polyline-encoded';
+export const decodePolyline = async (encoded) => {
+  const polyUtil = await import('polyline-encoded');
 
-export const decodePolyline = () => {
-  const latlngs = [
-    [38.5, -120.5],
-    [40.7, -120.95],
-    [43.252, -126.453]
-  ];
-
-  console.log(polyUtil.encode(latlngs));
-  const encoded = '_p~iF~cn~U_ulLn{vA_mqNvxq`@';
-  console.log(polyUtil.decode(encoded));
+  return polyUtil.decode(encoded);
 };
