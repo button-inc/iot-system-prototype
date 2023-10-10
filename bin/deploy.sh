7653776f29b7702d467295fce7707b8adbe321b9
YaokunLin <yaokun1@ualberta.ca>
2023-10-10 15:27:20 -0700

    modified-deploy-sh-to-incorpoare-docker-cleanup

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,9 +7,13 @@ PROJECT_ID="iot-portal-396300"
 
 # Check if we should update the .env file
 read -p "Do you want to update the api/.env file? (y/n): " UPDATE_ENV
+
+# Check if we should clean up Docker
+read -p "Do you want to clean up Docker (containers, images, volumes)? (y/n): " DOCKER_CLEANUP
+
+# Copy the .env file to the GCP Compute Engine instance
 if [ "$UPDATE_ENV" == "y" ]; then
     echo "copying api/.env..."
-    # Copy the .env file to the GCP Compute Engine instance
     gcloud compute scp ../api/.env mike@instance-3:~ --zone="$ZONE" --project="$PROJECT_ID"
 fi
 
@@ -20,6 +24,12 @@ gcloud compute ssh --zone "$ZONE" "$INSTANCE_NAME" --project "$PROJECT_ID" << EO
 if [ "$UPDATE_ENV" == "y" ]; then
     echo "moving the .env to the correct dir"
     sudo mv /home/mike/.env /home/mike/iot-system-prototype/api/.env
+fi
+
+# If we are cleaning up Docker, execute the cleanup command
+if [ "$DOCKER_CLEANUP" == "y" ]; then
+    echo "Cleaning up Docker..."
+    sudo docker system prune -a --volumes -f
 fi
 
 # Go to the project directory, lives in Mike's user profile
